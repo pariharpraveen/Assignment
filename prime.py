987d832767a570a631d946d73e703abc0ec99a40
pariharpraveen <115171003+pariharpraveen@users.noreply.github.com>
2022-10-07 20:25:30 +0530

    prime assignment

---
+++ prime.py
@@ -0,0 +1,14 @@
+def isPrime(n):
+  
+    # Corner case
+    if n <= 1:
+        return False
+  
+    # Check from 2 to n-1
+    for i in range(2, n):
+        if n % i == 0:
+            return False;
+  
+    return True
+print("Prime number") if isPrime(11) else print("Not a prime number")
+    
